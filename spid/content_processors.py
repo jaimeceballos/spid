c74ef7ae773d621aea390fba5541c39f87856f46
jaimeceballos <jaimeceballos82@gmail.com>
2014-09-04 13:55:43 -0300

    -

--- spid/content_processors.py
+++
@@ -1,6 +0,0 @@
-# importamos los settings del proyecto
-from django.conf import settings as _settings
-
-def settings(request):
-    # los devolvemos en la variable de contexto "settings"
-    return {'settings': _settings}
