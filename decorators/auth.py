624d4e76f8ec6b5a3a269bf249fd395705b0b7c8
jaime ceballos <jaimeceballos_rw@hotmail.com>
2016-04-14 14:01:06 -0300

    actualizar ultimo_ingreso

--- decorators/auth.py
+++ decorators/auth.py
@@ -1,20 +1,20 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-   
+
 from functools import wraps
-  
+
 from django.contrib.auth.models import Group,Permission,User
 from django.shortcuts import render, render_to_response,get_object_or_404
 from django.http import Http404
-   
-def group_required(groups=[]):    
+
+def group_required(groups=[]):
     def decorator(func):
         def inner_decorator(request,*args, **kwargs):
 
             for group in groups:
                 if Group.objects.get(name=group) in request.user.groups.all():
                    return func(request, *args, **kwargs)
-            
+
             return render(request,'./sinprivile.html')
 
         return wraps(func)(inner_decorator)
