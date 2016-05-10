87de3ed8225c6dd9faf14d88bcb8269ac236e7bd
jaime ceballos <jaimeceballos_rw@hotmail.com>
2016-05-10 11:13:49 -0300

    finalizado

--- NIF/urls.py
+++ NIF/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     url(r'^$',home_nif,name='home_nif'),
+    url(r'^generar/$',generar_codigos,name='generar'),
+    url(r'^descargar/$',descargar_codigos,name='descargar'),
 ]
--- NIF/views.py
+++ NIF/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, render_to_response
 from django.template.context import RequestContext
 from django.core import serializers
 from django.http import HttpResponse,HttpResponseRedirect, HttpResponse,Http404
+from django.core.urlresolvers import reverse
 from .forms import *
 from fpdf import FPDF
 import barcode, os, shutil
@@ -43,41 +44,59 @@ def completar(numero):
 	return numero
 
 def crear_barcode(numero):
-	filename = os.path.join('generated','temp',str(numero))
+	filename = os.path.join('NIF','static','generated','temp',str(numero))
 	print filename
 	writer = barcode.writer.ImageWriter()
 	code = barcode.Code39(numero,writer = writer,add_checksum = False)
 	archivo = code.save(filename)
 	return archivo
 
-def generar_codigos(provincia,ciudad,numeroInicial,cantidad):
-	total_imagenes = cantidad * 2
-	paginas =  calcular_cantidad_paginas(total_imagenes)
-	archivo = FPDF('P','mm','A4')
-	archivo.add_page()
-	for pagina in range(0,paginas):
-		eje_x = 0
-		for linea in range(0,12):
-			if(cantidad <= 0):
-				break
-			for codigo in range(0,2):
-				if(cantidad <= 0):
-					break
-				numero =  completar(str(numeroInicial))
-				numero = str(provincia) + str(ciudad) + numero
-				digito = digitoverificador(numero)
-				numero = numero + str(digito)
-				for imagen in range(0,2):
-					archivo.image(crear_barcode(numero),eje_x * 50, linea * 25 , TAMANIO_CODIGO)
-					eje_x += 1
-				cantidad = cantidad -1
-				numeroInicial += 1
-			eje_x = 0
-		if(cantidad > 0):
+def generar_codigos(request):
+	values = {}
+	form = GeneratorForm()
+	if request.method == 'POST':
+		form = GeneratorForm(request.POST)
+		if form.is_valid():
+			cantidad = int(form.cleaned_data['cantidad'])
+			provincia = form.cleaned_data['codigo_provincia']
+			ciudad = form.cleaned_data['codigo_ciudad']
+			numeroInicial = int(form.cleaned_data['numero_inicial'])
+			total_imagenes = int(cantidad) * 2
+			paginas =  calcular_cantidad_paginas(total_imagenes)
+			archivo = FPDF('P','mm','A4')
 			archivo.add_page()
-	archivo.output(os.path.join('generated','codigos.pdf'),'F')
-	shutil.rmtree(os.path.join('generated','temp'))
-
+			#os.remove(os.path.join('NIF','static','generated','codigos.pdf'))
+			os.makedirs(os.path.join('NIF','static','generated','temp'))
+			for pagina in range(0,paginas):
+				eje_x = 0
+				for linea in range(0,12):
+					if(cantidad <= 0):
+						break
+					for codigo in range(0,2):
+						if(cantidad <= 0):
+							break
+						numero =  completar(str(numeroInicial))
+						numero = str(provincia) + str(ciudad) + numero
+						digito = digitoverificador(numero)
+						numero = numero + str(digito)
+						for imagen in range(0,2):
+							archivo.image(crear_barcode(numero),eje_x * 50, linea * 25 , TAMANIO_CODIGO)
+							eje_x += 1
+						cantidad = cantidad -1
+						numeroInicial += 1
+					eje_x = 0
+				if(cantidad > 0):
+					archivo.add_page()
+			archivo.output(os.path.join('NIF','static','generated','codigos.pdf'),'F')
+			shutil.rmtree(os.path.join('NIF','static','generated','temp'))
+			return HttpResponseRedirect(reverse('descargar'))
+		else:
+			values['error'] = 'Error en el formulario vuelva a intentarlo'
+			values['form'] = form
+	else:
+		values['error'] = 'Error en el formulario vuelva a intentarlo'
+		values['form'] = form
+	return render_to_response('./index_nif.html',values,context_instance=RequestContext(request))
 def calcular_cantidad_paginas(cantidad):
 	resto = (cantidad / 48.0) - (cantidad / 48)
 	if( resto > 0 ):
@@ -95,3 +114,6 @@ def home_nif(request):
         'form' : form,
     }
     return render_to_response('./index_nif.html',values,context_instance = RequestContext(request))
+
+def descargar_codigos(request):
+	return render_to_response('descargar.html',{},context_instance= RequestContext(request))
